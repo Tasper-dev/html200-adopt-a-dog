31512b18f905af4c3e0b483a99b4b5c82e407288
Tasper <tiffanyasper@gmail.com>
2025-05-14 17:32:58 -0700

    added eventhandlers and some header styling

---
+++ adoptadog.js
@@ -0,0 +1,15 @@
+function dogDetails(dogName, dogBreed, adoptFee) {
+  result = dogName + " is " + dogBreed + "." + " Adoption fee is " + adoptFee;
+  alert(result);
+}
+
+function addFee(adoptFee) {
+  result = adoptFee + alert(result);
+}
+
+let total = 0;
+
+function addFee(adoptFee) {
+  total += adoptFee;
+  alert("Total is: $" + total.toFixed(2));
+}
