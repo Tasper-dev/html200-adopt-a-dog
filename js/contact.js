2c8f62ebc5c716900482d0015f0070386f6cb956
Tasper <tiffanyasper@gmail.com>
2025-06-01 21:08:23 -0700

    added js

--- js/contact.js
+++ js/contact.js
@@ -1,5 +1,42 @@
-const sendButton = document.querySelector("#sendButton");
+// const contactCardFields = [
+//   {
+//     name: "contactName",
+//   },
+//   {
+//     name: "contactCity",
+//   },
+//   {
+//     name: "contactEmail",
+//   },
+//   {
+//     name: "currentDogs",
+//   },
+//   {
+//     name: "prevAdopter",
+//   },
+//   {
+//     name: "custSurvery",
+//   },
+//   {
+//     name: "subject",
+//   },
+//   {
+//     name: "message",
+//   },
+// ];
 
-sendButton.addEventListener("click", function () {
+// const allFields = document.querySelectorAll("input, textarea");
+
+// for (let i = 0; i < contactCardFields.length; i++) {
+//   const contactData = contactCardFields[i];
+//   if (allFields[i]) {
+//     allFields[i].setAttribute("id", contactData.name);
+//     allFields[i].setAttribute("name", contactData.name);
+//   }
+// }
+
+const sendButton = document.querySelector("#sendButton");
+sendButton.addEventListener("click", function (event) {
+  event.preventDefault();
   alert("Thank you. The form information has been recieved.");
 });
