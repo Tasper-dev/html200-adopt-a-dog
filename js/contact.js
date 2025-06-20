e4491034c371187ebd3d5efa75837fcabbea3101
Tasper <tiffanyasper@gmail.com>
2025-06-20 12:44:01 -0700

    console logged inputs for contact form

--- js/contact.js
+++ js/contact.js
@@ -1,17 +1,13 @@
-//Retrieves for element
 const contactForm = document.querySelector("#contactForm");
 
-//Adds event listener
 contactForm.addEventListener("submit", function (event) {
   event.preventDefault();
 
-  //Selects all elements with class of input and returns a node list
   const inputs = document.querySelectorAll(".input");
 
-  //Iterates over the node list
   inputs.forEach((input) => {
     console.log(`Input name: ${input.name}, value: ${input.value}`);
   });
-  //Sends alert message on submit
+
   alert("Thank you. The form information has been recieved.");
 });
